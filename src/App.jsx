c8ef3fb727d9aef62ade6dc1e026b9c092b0eae3
Carlos <carlos_aperdomo@soy.sena.edu.co>
2025-04-01 16:47:56 -0500

    rroutes en la primera version

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import { Header } from './components/layouts/Header'
+import { AppRouter } from './routes/AppRouter'
+import { BrowserRouter } from 'react-router-dom'
 export const App = () => {
   return (
     <>
-      <Header />
+      <BrowserRouter>
+        <Header />
+        <AppRouter />
+      </BrowserRouter>
+      
 
     </>
   )
